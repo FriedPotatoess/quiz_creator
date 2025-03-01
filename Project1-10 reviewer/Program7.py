0a55539cc892e57bd63db00ae22764623cbf24f8
Jayvee <bustamantejayvee6@gmail.com>
2025-03-01 20:45:43 +0800

    added all the program for the reviewer

---
+++ Project1-10 reviewer/Program7.py
@@ -0,0 +1,20 @@
+numbers = []
+
+# Takes user input
+for i in range(10):
+    while True:  
+        user_input = input(f"Enter number {i + 1}: ")
+
+        # Anti float input
+        try:
+            num = int(user_input)
+            numbers.append(num)
+            break  # Exit the loop if the input is valid
+        except ValueError:
+            print("Please input an integer")
+
+# Counts the odd numbers
+odd_count = sum(1 for num in numbers if num % 2 != 0)
+
+# Output the odd numbers
+print(f"There are {odd_count} odd numbers in the numbers that you inputted.")
---
+++ Project1-10 reviewer/Program8.py
@@ -0,0 +1,19 @@
+numbers = []
+
+#Takes user input
+for i in range(10):
+    while True:  #Loop until a valid number is entered
+        user_input = input(f"Enter number {i + 1}: ")
+        
+        try:
+            num = float(user_input)  # Convert input to a float
+            numbers.append(num)  # Add the number to the list
+            break  # Exit the loop if input is valid
+        except ValueError:
+            print("Please input a valid number.")
+
+#Calculate the sum of the numbers
+total_sum = sum(numbers)
+
+#Outputs sum of all the numbers
+print(f"The sum of all the numbers is: {total_sum}")
