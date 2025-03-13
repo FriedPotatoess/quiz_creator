ee2c0106bdf49fc72cee1f5e18a5b83e35c9eba2
Jayvee <bustamantejayvee6@gmail.com>
2025-03-13 21:57:03 +0800

    added psuedocode and milestone for proj 4 batch 3

---
+++ Batch_3_Codes/Display_lowest_number.py
@@ -0,0 +1,6 @@
+# Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
+#Place to store the inputed numbers and the code to ask for the user to input numbers 
+#Function to track the input numbers until it is invalid, then print the lowest input
+
+
+
