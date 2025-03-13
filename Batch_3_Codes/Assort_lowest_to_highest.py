8c7405a2a46206e4d61d3a9af5bdd0dce77f37ab
Jayvee <bustamantejayvee6@gmail.com>
2025-03-13 22:11:50 +0800

    Added the psuedocode for the final one on batch 3

---
+++ Batch_3_Codes/Assort_lowest_to_highest.py
@@ -0,0 +1,6 @@
+# GOALS: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from lowest to highest. Clue: sort() function
+#Place to store the inputed numbers and the code to ask for the user to input numbers 
+#Function to track the input numbers until it is invalid, then assort the numbers from lowest to highest
+
+
+
