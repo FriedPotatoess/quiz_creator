369365ef3a7d071ab170049bc3f14118ad674f27
Jayvee <bustamantejayvee6@gmail.com>
2025-03-13 19:38:41 +0800

    Change the name and added the pseudocode

---
+++ Batch2reviewer/Smallernumberdecider.py
@@ -0,0 +1,12 @@
+#Create a function to input to numbers √
+#Create a deciding function to know what number is smaller √
+
+print("Input two numbers")
+
+num1 = input("")
+num2 = input("")
+
+if num1 > num2:
+    print(f"The smaller number is {num2}")
+if num2 > num1:
+    print(f"The smaller number is {num1}")  
