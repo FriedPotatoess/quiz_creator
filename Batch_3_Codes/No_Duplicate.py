1ec8fbd1c8123ca6b893bf6a70ecc96f0ad4239f
Jayvee <bustamantejayvee6@gmail.com>
2025-03-13 20:53:17 +0800

    Created Pseudocode for code 1 of Batch 3

---
+++ Batch_3_Codes/No_Duplicate.py
@@ -0,0 +1,3 @@
+#GOAL:Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
+#Place to store the inputed numbers and the code to ask for the user to input 10 numbers
+#Function that takes a list that tracks non duplicates
