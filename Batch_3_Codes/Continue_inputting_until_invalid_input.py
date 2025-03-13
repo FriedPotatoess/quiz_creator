d17f5e636aa86e50aec63ef2e948509a9c5c259e
Jayvee <bustamantejayvee6@gmail.com>
2025-03-13 21:39:50 +0800

    Started psuedocode for Prog3 and its milestone

---
+++ Batch_3_Codes/Continue_inputting_until_invalid_input.py
@@ -0,0 +1,5 @@
+#GOAL: Create a program that ask user to input a number, continue asking until the user input is invalid. Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when the inputted number have duplicate.
+#Place to store the inputed numbers and the code to ask for the user to input numbers 
+#Function to track the input numbers, say unique if its new and say duplicate if it has a duplicate
+
+
