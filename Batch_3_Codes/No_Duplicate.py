222c56e2ee8c4362da85e0fc3fe2d4e8d4f76d2d
Jayvee <bustamantejayvee6@gmail.com>
2025-03-13 21:10:27 +0800

    finished both milestone set

--- Batch_3_Codes/No_Duplicate.py
+++ Batch_3_Codes/No_Duplicate.py
@@ -1,11 +1,26 @@
 #GOAL:Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
-#Place to store the inputed numbers and the code to ask for the user to input 10 numbers √
-#Function that takes a list that tracks non duplicates
 
-num = []
+
+#Place to store the inputed numbers and the code to ask for the user to input 10 numbers √
+numbers = []
 
 print("Please input 10 numbers")
 
 for i in range(10):
     num = int(input(f"Enter number {i+1}: "))
     numbers.append(num)
+
+#list for non duplicates
+non_duplicates = []
+
+#Function that takes a list that tracks non duplicates √
+for num in numbers:
+    if numbers.count(num) == 1:
+        non_duplicates.append(num)
+        
+print(f"The following numbers that dont have any duplicates are:{non_duplicates}")
+        
+
+
+
+    
