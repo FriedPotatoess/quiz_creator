8eaeeda39001e62d30f1f995814369a78872f1d2
Jayvee <bustamantejayvee6@gmail.com>
2025-03-13 22:06:02 +0800

    Finished code, with all of its milestone reached succesfully

--- Batch_3_Codes/Display_lowest_number.py
+++ Batch_3_Codes/Display_lowest_number.py
@@ -1,6 +1,28 @@
 # Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
-#Place to store the inputed numbers and the code to ask for the user to input numbers 
+#Place to store the inputed numbers and the code to ask for the user to input numbers √
+#Function to track the input numbers until it is invalid, then print the lowest input √
+
+#Place to store the inputed numbers and the code to ask for the user to input numbers
+#list
+numbers = []
+
+while True:
+    user_input = input("Enter a number(Enter a non-number to stop): ")
+        
 #Function to track the input numbers until it is invalid, then print the lowest input
-
-
-
+    try:
+        num = float(user_input)
+        
+        if numbers.count(num) == 0:
+            print("Unique")
+        else:
+            print ("Duplicate")
+        numbers.append(num)
+        
+    except ValueError:
+        if numbers:
+            lowest_number = min(numbers)
+            print(f"The lowest number inputted is {lowest_number}")
+        else:
+            print("No valid numbers were entered")
+        break
