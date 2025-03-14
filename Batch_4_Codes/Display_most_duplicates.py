a6aafb5d67950df601d90f5a87c0555a4c1c5824
Jayvee <bustamantejayvee6@gmail.com>
2025-03-14 19:00:38 +0800

    completed first milestone

--- Batch_4_Codes/Display_most_duplicates.py
+++ Batch_4_Codes/Display_most_duplicates.py
@@ -1,5 +1,13 @@
 #GOALS: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
-#Place to store the inputed numbers and the code to ask for the user to input numbers until they put an invalid number
+#Place to store the inputed numbers and the code to ask for the user to input numbers until they put an invalid number √
 #Create a function to track all the input numbers 
 #Create a function that keep tracks of duplicate
 #Create a function that prints the number with the most duplicate
+
+#Place to store the inputed numbers and the code to ask for the user to input numbers until they put an invalid number
+#List
+numbers = []
+
+while True:
+    user_input = input("Enter a number(Enter a non-number to stop): ")
+    
