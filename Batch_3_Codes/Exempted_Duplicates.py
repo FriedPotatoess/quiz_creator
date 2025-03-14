34ea7a5767422a0f45442632b9bb59c1b8726323
Jayvee <bustamantejayvee6@gmail.com>
2025-03-14 18:45:56 +0800

    started on batch 4, added milestone for prog 1

--- Batch_3_Codes/Exempted_Duplicates.py
+++ Batch_3_Codes/Exempted_Duplicates.py
@@ -5,7 +5,7 @@ numbers = []
 print("Please input 10 numbers")
 
 for i in range(10):
-    num = int(input(f"Enter number {i+1}: "))
+    num = float(input(f"Enter number {i+1}: "))
     numbers.append(num)
 
 #List for numbers without duplicate and first instance of duplicate numbers
