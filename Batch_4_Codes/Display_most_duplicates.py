d5bec61d2b05068be428226d6008f59c1e994b1a
Jayvee <bustamantejayvee6@gmail.com>
2025-03-14 18:58:50 +0800

    added pseudocode and milestone for code 2

---
+++ Batch_4_Codes/Display_most_duplicates.py
@@ -0,0 +1,5 @@
+#GOALS: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
+#Place to store the inputed numbers and the code to ask for the user to input numbers until they put an invalid number
+#Create a function to track all the input numbers 
+#Create a function that keep tracks of duplicate
+#Create a function that prints the number with the most duplicate
