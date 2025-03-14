7976375d0448429e25ee6af420d128be6732f3fc
Jayvee <bustamantejayvee6@gmail.com>
2025-03-14 19:47:54 +0800

    Completed milestones for program 4, its fast cuz i just reversed the same code for batch 3

--- Batch_4_Codes/Assort__highest_to_lowest.py
+++ Batch_4_Codes/Assort__highest_to_lowest.py
@@ -1,6 +1,6 @@
 #GOALS: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from highest to lowest. Clue: sort() function
-#Place to store the inputed numbers and the code to ask for the user to input numbers until they put an invalid number 
-#Create a function that prints from highest to lowest
+#Place to store the inputed numbers and the code to ask for the user to input numbers until they put an invalid number  √
+#Create a function that prints from highest to lowest √
 
 #Place to store the inputed numbers and the code to ask for the user to input numbers
 #list
@@ -22,6 +22,6 @@ while True:
 #Assort the numbers from lowest to highest
     except ValueError:
         print("Invalid input. Program now ending")
-        numbers.sort()
+        numbers.sort(reverse = True)
         print(f"Numbers from lowest to highest: {numbers}")
         break
