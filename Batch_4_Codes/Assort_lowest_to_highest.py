fbf91ca6fc273675c393027529617c037e0a0cbc
Jayvee <bustamantejayvee6@gmail.com>
2025-03-14 19:40:29 +0800

    added milestone for program 4

---
+++ Batch_4_Codes/Assort_lowest_to_highest.py
@@ -0,0 +1,4 @@
+#GOALS: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from highest to lowest. Clue: sort() function
+#Place to store the inputed numbers and the code to ask for the user to input numbers until they put an invalid number 
+#Create a function that prints from highest to lowest
+
