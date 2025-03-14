e4ed8a1b8c0da8e98854a7960f94aacbf926e608
Jayvee <bustamantejayvee6@gmail.com>
2025-03-14 20:00:21 +0800

    completed first milestone

--- Batch_4_Codes/Average_of_all_input.py
+++ Batch_4_Codes/Average_of_all_input.py
@@ -1,3 +1,17 @@
 #GOALS: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
-#Place to store the inputed numbers and the code to ask for the user to input numbers 
+#Place to store the inputed numbers and the code to ask for the user to input numbers until they put an invalid number
 #Function to track the input numbers until it is invalid, then get the average of all the numbers
+
+#List
+numbers = []
+
+#Place to store the inputed numbers and the code to ask for the user to input numbers until they put an invalid number
+while True:
+    user_input = input("Enter a number(Enter a non-number to stop): ")
+    
+    try:
+        num = float(user_input)
+        numbers.append(numbers)
+    except ValueError:
+        print("Invalid input, terminating program")
+        break
