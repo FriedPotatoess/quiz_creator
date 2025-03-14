d0d7154a7125657a7cbb4fc407ec3c0e14342fbb
Jayvee <bustamantejayvee6@gmail.com>
2025-03-14 19:22:27 +0800

    added milestones for program 3

---
+++ Batch_4_Codes/Print_highest_number.py
@@ -0,0 +1,8 @@
+#GOAL: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number
+#Place to store the inputed numbers and the code to ask for the user to input numbers until they put an invalid number
+#Create a function that prints the highest number
+
+#List
+numbers = []
+
+
