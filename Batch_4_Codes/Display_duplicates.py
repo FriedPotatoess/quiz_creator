9fac5162e90a2d845846f130458faa0ac3a1d204
Jayvee <bustamantejayvee6@gmail.com>
2025-03-14 18:47:48 +0800

    completed milestone 1

---
+++ Batch_4_Codes/Display_duplicates.py
@@ -0,0 +1,7 @@
+# GOALS: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
+#Place to store the inputed numbers and the code to ask for the user to input numbers √
+#Create a function to track all the input numbers
+#Create a function that prints all numbers that have duplicates
+
+
+    
