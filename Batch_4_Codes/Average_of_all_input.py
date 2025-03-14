9a781c09f94545d3b10d3d3e87c72fb106bd0674
Jayvee <bustamantejayvee6@gmail.com>
2025-03-14 19:56:45 +0800

    Added milestone for the last program

---
+++ Batch_4_Codes/Average_of_all_input.py
@@ -0,0 +1,3 @@
+#GOALS: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
+#Place to store the inputed numbers and the code to ask for the user to input numbers 
+#Function to track the input numbers until it is invalid, then get the average of all the numbers
