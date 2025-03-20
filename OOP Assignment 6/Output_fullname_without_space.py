072042f04971cbedf31bc68695b20252e6650ea6
Jayvee <bustamantejayvee6@gmail.com>
2025-03-20 21:08:29 +0800

    started working on code 1 of assignment 6, added milestone to be reached, which is simple except  the deleting the spaces part, have to research it

---
+++ OOP Assignment 6/Output_fullname_without_space.py
@@ -0,0 +1,3 @@
+#GOALS: Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
+#Ask for the input of the username
+#Figure out how to get only the useable input ex. no space 
