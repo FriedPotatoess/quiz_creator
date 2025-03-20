d9d9b24c87389c564a8b1f45bf32097feabd5c72
Jayvee <bustamantejayvee6@gmail.com>
2025-03-20 21:53:27 +0800

    Figured out how to do the task(googled it)and implemented it

--- OOP Assignment 6/Output_fullname_without_space.py
+++ OOP Assignment 6/Output_fullname_without_space.py
@@ -1,3 +1,10 @@
 #GOALS: Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
-#Ask for the input of the username
-#Figure out how to get only the useable input ex. no space 
+#Ask for the input of the fullname √
+#Figure out how to get only the useable input ex. no space √
+
+#Asks for input
+fullname = input("Please insert your full name")
+
+#Cleans input
+cleaned_fullname = fullname.lstrip()
+print(cleaned_fullname)
