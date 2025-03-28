9876da55d72e9fc56be5b0e9ae40d67cc8c88dd3
Jayvee <bustamantejayvee6@gmail.com>
2025-03-28 20:40:45 +0800

    started work on code 2

---
+++ OOP Assignment 6/Output_fullname_in_capslock.py
@@ -0,0 +1,4 @@
+ #Goals:Create a program that ask the user to input their fullname. Print the input in all capital letter.
+
+#Get the user input
+user_input = input("Please input your Full name: ")
