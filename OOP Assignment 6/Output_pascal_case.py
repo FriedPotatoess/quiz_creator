cf77356ba5b2395166216e73231b3e2f1a48baa6
Jayvee <bustamantejayvee6@gmail.com>
2025-03-28 21:36:30 +0800

    started work on the final program of batch 5

---
+++ OOP Assignment 6/Output_pascal_case.py
@@ -0,0 +1,4 @@
+# Create a program that ask the user to input their fullname in incorrect casing. Print the input in pascal case.
+
+#Get the user input
+user_input = input("Please input your Full name: ")
