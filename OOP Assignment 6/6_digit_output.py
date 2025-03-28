70da60436f5935db39f1dc0bf716ba70bbd06f5e
Jayvee <bustamantejayvee6@gmail.com>
2025-03-28 20:05:58 +0800

    added milestone for batch 5 program 2

---
+++ OOP Assignment 6/6_digit_output.py
@@ -0,0 +1,4 @@
+#GOALS: Create a program that ask the user to input a number (0-1000). Print the number in 6 digit format. Add zeros at the beginning to complete the 6 digit.
+#Create a fucntion to get the user input
+#Create a fucntion to make the output 6 digits
+
