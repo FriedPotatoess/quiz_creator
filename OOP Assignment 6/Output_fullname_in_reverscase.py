2f9dca3be6947517534c276b8efaa1c3bfdfebaf
Jayvee <bustamantejayvee6@gmail.com>
2025-03-28 20:58:20 +0800

    started work on prog6

---
+++ OOP Assignment 6/Output_fullname_in_reverscase.py
@@ -0,0 +1,4 @@
+# Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
+
+#Get the user input
+user_input = input("Please input your Full name: ")
