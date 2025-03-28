f1b9c13249c5121bb7d9de7d27a6cac25d6375a7
Jayvee <bustamantejayvee6@gmail.com>
2025-03-28 20:09:06 +0800

    completed first milestone

--- OOP Assignment 6/6_digit_output.py
+++ OOP Assignment 6/6_digit_output.py
@@ -1,4 +1,5 @@
 #GOALS: Create a program that ask the user to input a number (0-1000). Print the number in 6 digit format. Add zeros at the beginning to complete the 6 digit.
-#Create a fucntion to get the user input
-#Create a fucntion to make the output 6 digits
+#Create a fucntion to get the user input √
+#Create a fucntion to make the output 6 digits 
 
+user_input = int(input("Please input your digits: "))
