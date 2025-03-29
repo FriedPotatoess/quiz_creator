95c5b5329011011fd2f37f1027d7afce37753f75
FriedPotatoess <bustamantejayvee6@gmail.com>
2025-03-29 15:31:08 +0800

    Delete OOP assignment 2-5 directory

--- OOP assignment 2-5/Batch_1_Codes/Even_number_for_loop.py
+++
@@ -1,5 +0,0 @@
-#Create a for loop to output even numbers
-#thats it
-
-for num in range(0, 101, 2):
-    print(num)
--- OOP assignment 2-5/Batch_2_Codes/Odd_number_printer.py
+++
@@ -1,8 +0,0 @@
-#Define the initial count of num √
-#Create a function that prints odd number √
-
-num = 1
-
-while num <= 100:
-    print(num)
-    num += 2
