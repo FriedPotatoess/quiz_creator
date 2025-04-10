440be2835c6f4d6ad9b07b955fb3c1eb0414da79
Jayvee <bustamantejayvee6@gmail.com>
2025-04-10 19:26:50 +0800

    started working on assignment 9

--- Quiz Maker/quiz_maker.py
+++ Quiz Maker/quiz_maker.py
@@ -1,2 +1,2 @@
-# Create a program that ask user for a question, it will also ask for 4 possible answer (a,b,c,d) and the correct answer. Write the collected data to a text file. Ask another question until the user chose to exit.
-#Objectives: 1. Create a program that ask user to create a quiz.  2.Create a way for that quiz to be saved as a txt file
+#Create a program that ask user for a question, it will also ask for 4 possible answer (a,b,c,d) and the correct answer. Write the collected data to a text file. Ask another question until the user chose to exit.
+#GOALS: 1: Create a program that lets user create a quiz 2: Create a way for that file to be saved in the user folder
