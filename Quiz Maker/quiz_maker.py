fe0dcdbfa1dc5a78aba1e98bc179fc5cbd890e1d
Jayvee <bustamantejayvee6@gmail.com>
2025-04-10 20:18:28 +0800

    Finished on the first goal, which is to create a program to allow user to create a quiz, using while loop to ensure that if the user wanted to input more questions, they can do so. Now proceeding to work on how to store the quiz onto a txt file

--- Quiz Maker/quiz_maker.py
+++ Quiz Maker/quiz_maker.py
@@ -1,2 +1,31 @@
 #Create a program that ask user for a question, it will also ask for 4 possible answer (a,b,c,d) and the correct answer. Write the collected data to a text file. Ask another question until the user chose to exit.
 #GOALS: 1: Create a program that lets user create a quiz 2: Create a way for that file to be saved in the user folder
+
+#Quiz Creator
+def quiz_maker():
+    while True:
+        questions = input("Enter your Question: ")
+    
+        options = {}
+        for option in ['A', 'B', 'C', 'D']:
+            options[option] = input(f"{option}: ")
+            
+        correct_answer = input("Which letter is the correct answer?(A, B, C, or, D): ").upper()
+        print(f"Question: {questions}")
+        print("Choices: ")
+        for option, answer in options.items():
+            print(f"{option}: {answer}")
+        print(f"Correct Answer: {correct_answer}\n")
+        
+        while True:
+            continue_questioning = input("Enter another question? (Yes/No): ").capitalize()
+            if continue_questioning == "Yes":
+                break
+            elif continue_questioning == "No":
+                print("Thank you for using the Quiz Maker, now exiting...")
+                return
+            else:
+                print('Please choose between "Yes" or "No"')
+        
+quiz_maker()
+    
