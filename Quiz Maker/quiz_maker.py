927dda37cb497bdc3e6c64b14d34254b30ffae36
Jayvee <bustamantejayvee6@gmail.com>
2025-04-10 21:09:40 +0800

    Finished the last milestone, which is to let the quiz created be saved as a txt file. Achieved that by creating another function named save_file, adding all the variables such as the questions, the choices and the answer.The function contatins all the stuff that is required to handle the file such as file write which writes the string that is inside the variable(for lack of better wording) and all the good stuff that i found in Module 2: File handling. i used the a(append)permission so that it can be changed but not overwrite, instead adding the questions in there as part of that file. Will start working on the demo tommorow as i am running out of time

--- Quiz Maker/quiz_maker.py
+++ Quiz Maker/quiz_maker.py
@@ -1,22 +1,40 @@
 #Create a program that ask user for a question, it will also ask for 4 possible answer (a,b,c,d) and the correct answer. Write the collected data to a text file. Ask another question until the user chose to exit.
-#GOALS: 1: Create a program that lets user create a quiz 2: Create a way for that file to be saved in the user folder
+#GOALS: √1: Create a program that lets user create a quiz 2: Create a way for that file to be saved in the user folder
+
+def save_file(questions, options, correct_answer, test_name):       
+    with open(test_name, "a") as file:
+        file.write(f"Question: {questions}\n")
+        for option, answer in options.items():
+            file.write(f"{option}: {answer}\n")
+        file.write(f"Correct Answer: {correct_answer}\n\n")
+
+
 
 #Quiz Creator
 def quiz_maker():
+    test_name = input("What would be the name of this here's quiz?: ")
+    if not test_name.endswith(".txt"):
+        test_name += ".txt"  
+   
     while True:
         questions = input("Enter your Question: ")
-    
+        #Stores the answer    
         options = {}
         for option in ['A', 'B', 'C', 'D']:
             options[option] = input(f"{option}: ")
-            
+        #Determines the correct answer   
         correct_answer = input("Which letter is the correct answer?(A, B, C, or, D): ").upper()
+        #Reiterates your question, the choices and the correct answer
         print(f"Question: {questions}")
         print("Choices: ")
         for option, answer in options.items():
             print(f"{option}: {answer}")
         print(f"Correct Answer: {correct_answer}\n")
         
+        #Calls the save_file function to save the text already placed, before asking the user if they want to repeat the process again
+        save_file(questions, options, correct_answer, test_name)
+        
+        #Asks the user if they want to input another set of question
         while True:
             continue_questioning = input("Enter another question? (Yes/No): ").capitalize()
             if continue_questioning == "Yes":
