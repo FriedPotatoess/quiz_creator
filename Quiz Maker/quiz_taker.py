d2732af27d78f49fc84c66a2c70f78699261229c
Jayvee <bustamantejayvee6@gmail.com>
2025-05-01 02:18:08 +0800

    started working on a function that will initialize the quiz, havent, still incomplete. The commit gap was so long because i was procrastinating

--- Quiz Maker/quiz_taker.py
+++ Quiz Maker/quiz_taker.py
@@ -4,3 +4,19 @@
 #2. Probably randomize the question and place a score and correct answer at the start
 #3. Beautify
 
+
+def init_quiz(filename):
+    questions = []
+    try:
+        with open(filename, r) as file:
+            while True:
+                question_line=file.readline()
+                if not question_line:
+                    break
+                
+                question = question_line.split(":", 1)[1].strip()
+
+                options = {}
+                for option in ['A', 'B', 'C', 'D']:
+                    option_line = file.readline().strip()
+                    options[option] = option_line.split(":", 1)[1].strip()
