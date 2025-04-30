95de351c70d6ab97f7a3f2de9163e9b0d12748da
Jayvee <bustamantejayvee6@gmail.com>
2025-04-30 22:55:15 +0800

    started working on the quiz taker assignment, placed the goals and what i want to do

---
+++ Quiz Maker/quiz_taker.py
@@ -0,0 +1,6 @@
+#Goal: Create the Quiz program that read the output file of the Quiz Creator. The user will answer the randomly selected question and check if the answer is correct.
+#Milestones:
+#1. Create a Program that pull a text file from the folder by asking the name of the test
+#2. Probably randomize the question and place a score and correct answer at the start
+#3. Beautify
+
